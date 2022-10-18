86b77202c4c0378dda79ee07192cfad906b5075f
Pierre-Yves Aillet <pyaillet@gmail.com>
2022-10-18 17:17:45 +0200

    fix gitignore

---
+++ main/assets/DrawingControls.d560de78.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.80c53ec0.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.80c53ec0.js";import"./index.8bc23e99.js";export{t as default};
---
+++ main/assets/DrawingPreview.c3fbf759.js
@@ -0,0 +1,1 @@
+import"./index.8bc23e99.js";import{ai as o}from"./index.8bc23e99.js";export{o as default};
